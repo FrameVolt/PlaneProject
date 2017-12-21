acc6179d76439e94ed91efb80ff789f69b40ce41
Yaom!ng <1064004998@qq.com>
2017-12-21 22:44:27 +0800

    Add LevelLoading
    add two scenes, level fading, uimanager

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : Singleton<UIManager>
+{
+    [SerializeField]
+    private Image fader;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (fader != null)
+            fader.gameObject.SetActive(false);
+    }
+    public virtual void FaderOn(bool state, float duration)
+    {
+        if (fader != null)
+        {
+            fader.gameObject.SetActive(true);
+            if (state)
+                StartCoroutine(FadeImage(fader, duration, new Color(0, 0, 0, 1f)));
+            else
+                StartCoroutine(FadeImage(fader, duration, new Color(0, 0, 0, 0f)));
+        }
+    }
+
+    public static IEnumerator FadeImage(Image image, float duration, Color color)
+    {
+        if (image == null)
+            yield break;
+
+        float alpha = image.color.a;
+
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / duration)
+        {
+            if (image == null)
+                yield break;
+            Color newColor = new Color(color.r, color.g, color.b, Mathf.SmoothStep(alpha, color.a, t));
+            image.color = newColor;
+            yield return null;
+        }
+        image.color = color;
+    }
+}
